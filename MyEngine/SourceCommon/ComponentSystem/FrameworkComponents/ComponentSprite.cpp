154ef94615f723ff41c31d2104180589e3c88a7f
Jimmy Lord <jimmylord@gmail.com>
2016-06-02 21:13:13 -0400

    Updates to match framework changes.

--- MyEngine/SourceCommon/ComponentSystem/FrameworkComponents/ComponentSprite.cpp
+++ MyEngine/SourceCommon/ComponentSystem/FrameworkComponents/ComponentSprite.cpp
@@ -317,7 +317,7 @@ void ComponentSprite::DrawCallback(ComponentCamera* pCamera, MyMatrix* pMatViewP
     ComponentRenderable::Draw( pMatViewProj, pShaderOverride, 0 );
 
     //m_pSprite->SetPosition( m_pComponentTransform->GetWorldTransform() );
-    m_pSprite->SetTint( m_Tint );
+    //m_pSprite->SetTint( m_Tint );
     //m_pSprite->Create( "ComponentSprite", m_Size.x, m_Size.y, 0, 1, 0, 1, Justify_Center, false );
     m_pSprite->Draw( m_pComponentTransform->GetWorldTransform(), pMatViewProj, pShaderOverride );
 }
